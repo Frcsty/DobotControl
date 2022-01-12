025d785035ce2a3a127021c7c215e06c15b56d9c
mark.prenar <mark.prenar@NB-DEL351-802R>
2022-01-12 13:19:47 +0100

    Add project files.

---
+++ DobotControl.ino
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "Protocol.h"
+#include "command.h"
+#include "FlexiTimer2.h"
+
+//Set Serial TX&RX Buffer Size
+#define SERIAL_TX_BUFFER_SIZE 64
+#define SERIAL_RX_BUFFER_SIZE 256
+
+
